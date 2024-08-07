107ceb006af3d89369c3f4bfb153b3cf19db0d69
TusharMagdum1510 <149376030+TusharMagdum1510@users.noreply.github.com>
2024-08-07 11:05:22 +0530

    added UserForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import UserForm from "./UserForm";
+
+export interface UsersType {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<UsersType[]>([]);
+
+  const onUserAdd = (user:UsersType) => {
+    setUsers([...users, user]);
+  };
 
   return (
     <>
-
+      <UserForm onUserAdd={onUserAdd} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
